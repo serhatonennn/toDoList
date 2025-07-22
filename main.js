400c89927a18be69673a78c4c402e8a59647e4b9
serhatonennn <serhat.hsn@gmail.com>
2025-07-22 11:02:14 +0300

    Files

--- main.js
+++ main.js
@@ -2,18 +2,31 @@ const addForm = document.getElementById("addForm");
 const input = document.querySelector('input[name="addToDo"]');
 const todoList = document.getElementById("todoList");
 
+
 addForm.addEventListener("submit", function(e) {
     e.preventDefault();
 
     const taskText = input.value;
     const newitem = document.createElement("div");
-    newitem.className = "list-group-item d-flex justify-content-between align-items-center text-l";
-    newitem.innerHTML = `
-        <span>${taskText}</span>
+
+    if (taskText === "") {
+        alert("Boş Girilemez.");
+    } else {
+        const taskId = `taskDone${Date.now()}`;
+        newitem.className = "list-group-item d-flex justify-content-between align-items-center text-l text-danger";
+        newitem.innerHTML = `
+    <span>${taskText}</span>
+    <div class="d-flex align-items-center gap-2">
+        <input type="checkbox" class="btn-check" id="${taskId}" autocomplete="off">
+        <label class="btn btn-outline-success btn-sm" for="${taskId}">Tamamlandı</label>
         <button class="btn btn-outline-danger btn-sm delete-btn">Sil</button>
-    `;
-    todoList.appendChild(newitem);
+    </div>
+`;
+        input.value = "";
+        todoList.appendChild(newitem);
+    }
 });
+
 
 todoList.addEventListener("click", function(e) {
     if (e.target.classList.contains("delete-btn")) {
@@ -21,9 +34,13 @@ todoList.addEventListener("click", function(e) {
     }
 });
 
-document.addEventListener("click", function(e) {
-    if (e.target.tagName === "SPAN" && e.target.closest(".list-group-item")) {
-        e.target.classList.toggle("text-decoration-line-through ");
+todoList.addEventListener("change", function(e) {
+    if (e.target.type === "checkbox") {
+        const listItem = e.target.closest(".list-group-item");
+        const span = listItem.querySelector("span");
+
+        span.classList.toggle("text-success", e.target.checked);
+        span.classList.toggle("text-decoration-line-through", e.target.checked);
     }
 });
 
